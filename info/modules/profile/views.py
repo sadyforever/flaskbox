116582e5dbb9ef0a188fb31e3a6c97f41c3bb6fb
sunsady <405363219@qq.com>
2018-04-26 16:17:09 +0800

    上传头像&修改密码&收藏的新闻

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,10 +1,12 @@
-from flask import g, redirect, render_template, request, jsonify
+from flask import g, redirect, render_template, request, jsonify, current_app
 
+from info import constants
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.image_storage import storage
 from info.utils.response_code import RET
 
-
+# 如果没登录跳回首页
 @profile_blu.route('/info')
 @user_login_data
 def user_info():
@@ -17,7 +19,7 @@ def user_info():
 
 
 
-
+# 个人中心基本信息展示,默认第一个框
 @profile_blu.route('/base_info', methods=["GET", "POST"])
 @user_login_data
 def base_info():
@@ -57,9 +59,106 @@ def base_info():
 '''
 
 
+# 上传头像
+@profile_blu.route('/pic_info', methods=["GET", "POST"])
+@user_login_data
+def pic_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_pic_info.html', data={"user": user.to_dict()})
+
+    # 如果是POST表示修改头像
+    # 1. 取到上传的图片
+    try:
+        # 无论什么格式的媒体文件,都是二进制传过来的
+        avatar = request.files.get("avatar").read()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 2. 上传头像
+    try:
+        # 使用自已封装的storage方法去进行图片上传
+        # 第三方图片储存平台,我们上传图片,每个图片返回一个key,然后和网址提供的链接前缀拼接构成完整的图片地址,给用户返回的时候返回这个地址,让用户去请求外网
+        # 在数据库中我们只需要储存key就可以
+        key = storage(avatar)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.THIRDERR, errmsg="上传头像失败")
+
+    # 3. 保存头像地址
+    user.avatar_url = key
+    return jsonify(errno=RET.OK, errmsg="OK", data={"avatar_url": constants.QINIU_DOMIN_PREFIX + key})
 
 
 
+# 更改密码
+@profile_blu.route('/pass_info', methods=["GET", "POST"])
+@user_login_data
+def pass_info():
+    if request.method == "GET":
+        return render_template('news/user_pass_info.html')
+
+    # 1. 获取参数
+    old_password = request.json.get("old_password")
+    news_password = request.json.get("new_password")
+
+    # 2. 校验参数
+    if not all([old_password, news_password]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 3. 判断旧密码是否正确
+    user = g.user
+    if not user.check_password(old_password):
+        return jsonify(errno=RET.PWDERR, errmsg="原密码错误")
+
+    # 4. 设置新密码
+    user.password = news_password
+
+    return jsonify(errno=RET.OK, errmsg="保存成功")
+
+
+
+# 用户收藏的新闻
+@profile_blu.route('/collection')
+@user_login_data
+def user_collection():
+
+    # 获取参数
+    page = request.args.get("p", 1)
+
+    # 判断参数
+    try:
+        page = int(page)
+    except Exception as e:
+        current_app.logger.error(e)
+        page = 1
+
+    # 查询用户指定页数的收藏的新闻
+    user = g.user
+
+    news_list = []
+    total_page = 1
+    current_page = 1
+    try:
+        paginate = user.collection_news.paginate(page, constants.USER_COLLECTION_MAX_NEWS, False)
+        current_page = paginate.page
+        total_page = paginate.pages
+        news_list = paginate.items
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_li = []
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
+
+    data = {
+        "total_page": total_page,
+        "current_page": current_page,
+        "collections": news_dict_li
+    }
+
+    return render_template('news/user_collection.html', data=data)
 
 
 
--- info/static/news/js/user_pic_info.js
+++ info/static/news/js/user_pic_info.js
@@ -9,5 +9,22 @@ $(function () {
         e.preventDefault()
 
         //TODO 上传头像
+        // ajaxSubmit 代表使用 jquery 的代码去模拟点击提交按钮的操作
+        $(this).ajaxSubmit({
+            url: "/user/pic_info",
+            type: "POST",
+            headers: {
+                "X-CSRFToken": getCookie('csrf_token')
+            },
+            success: function (resp) {
+                if (resp.errno == "0") {
+                    $(".now_user_pic").attr("src", resp.data.avatar_url)
+                    $(".user_center_pic>img", parent.document).attr("src", resp.data.avatar_url)
+                    $(".user_login>img", parent.document).attr("src", resp.data.avatar_url)
+                }else {
+                    alert(resp.errmsg)
+                }
+            }
+        })
     })
 })
